45de76670bd3c3a88b997832c77c817f5301c2ee
Sidak <sidak.pasrichassp@gmail.com>
2018-05-11 15:23:07 +0100

    Added filterList function
    Updates values of numberOfTimesLearned and removes Words never learned

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Lingumi
 {
@@ -6,8 +8,51 @@ namespace Lingumi
     {
         static void Main(string[] args)
         {
-            Word a = new Word("a", false, 3);
-            Console.WriteLine(a.ToString());
+            Word a = new Word("apple", false, 2);
+            Word b = new Word("bad", false, 0);
+            Word c = new Word("cat", true, 5);
+            Word d = new Word("dog", false, 0);
+
+            Word[] words = { a, b, c, d };
+
+            String[] ids = { "bad", "bad", "cat", "pus" };
+
+            Word[] result = filterList(new List<Word>(words), new List<string>(ids));
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+            Console.ReadKey();
+        }
+
+        public void sendStickers(List<Word> words, List<string> wordIds)
+        {
+
+        }
+
+        private static Word[] filterList(List<Word> words, List<string> wordIds)
+        {
+            List<Word> filteredList = new List<Word>();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                Word currentWord = words[i];
+                for (int j = 0; j < wordIds.Count; j++)
+                {
+                    String currentID = wordIds[j];
+                    if (currentWord.getId().Equals(currentID))
+                    {
+                        currentWord.setNumberOfTimesLearned(currentWord.getNumberOfTimesLearned() + 1);
+                    }
+                }
+                if (currentWord.getNumberOfTimesLearned() > 0)
+                {
+                    filteredList.Add(currentWord);
+                }
+            }
+
+            Word[] toReturn = filteredList.ToArray();
+            return toReturn;
         }
     }
 }
