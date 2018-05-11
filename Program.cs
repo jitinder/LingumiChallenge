85f3f9cc69394a53171d8fd4288951ec186bb1fb
Sidak <sidak.pasrichassp@gmail.com>
2018-05-11 14:46:44 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lingumi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Word a = new Word("a", false, 3);
+            Console.WriteLine(a.ToString());
+        }
+    }
+}
---
+++ Word.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lingumi
+{
+    class Word
+    {
+        private string id;
+        private bool hasAlreadyCollected;
+        private int numberOfTimesLearned;
+
+        public Word(string id, bool hasAlreadyCollected, int numberOfTimesLearned)
+        {
+            this.id = id;
+            this.hasAlreadyCollected = hasAlreadyCollected;
+            this.numberOfTimesLearned = numberOfTimesLearned;
+        }
+
+        public string getId()
+        {
+            return this.id;
+        }
+
+        public bool isAlreadyCollected()
+        {
+            return this.hasAlreadyCollected;
+        }
+
+        public int getNumberOfTimesLearned()
+        {
+            return this.numberOfTimesLearned;
+        }
+
+        public void setId(string id)
+        {
+            this.id = id;
+        }
+
+        public void setCollected(bool hasAlreadyCollected)
+        {
+            this.hasAlreadyCollected = hasAlreadyCollected;
+        }
+
+        public void setNumberOfTimesLearned(int numberOfTimesLearned)
+        {
+            this.numberOfTimesLearned = numberOfTimesLearned;
+        }
+        
+        override
+        public string ToString()
+        {
+            return "ID = " + id + " Collected? " + hasAlreadyCollected + " Learnt " + numberOfTimesLearned + " times";
+        }
+    }
+}
